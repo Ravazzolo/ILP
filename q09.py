cbb76453973e1480164319d39af6322d9b1c7154
Ravazzolo <gabrielravazzolo@gmail.com>
2026-04-10 16:34:30 +0000

    questoes

---
+++ q09.py
@@ -0,0 +1,27 @@
+n1=int(input("digite o mes: "))
+if n1==1:
+    print("Janeiro")
+elif n1==2:
+    print("fevereiro")
+elif n1==3:
+    print("março")
+elif n1==4:
+    print("abril")
+elif n1==5:
+    print("maio")
+elif n1==6:
+    print("junho")
+elif n1==7:
+    print("julho")
+elif n1==8:
+    print("agosto")
+elif n1==9:
+    print("setembro")
+elif n1==10:
+    print("outubro")
+elif n1==11:
+    print("novembro")
+elif n1==12:
+    print("Dezembro")
+else:
+    print("Valor inválido")
